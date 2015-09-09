4ccd672de51910a5366deef6461b865bcd43c291
Student <student@RU-109.local>
2015-09-09 16:02:58 -0600

    3rd

--- introang.js
+++ introang.js
@@ -1,9 +1,28 @@
+// angular.module("button", [])
+
+
+// var buttonController = function ($scope){
+// 	$scope.click = function(){
+// 		console.log("You clicked me!");
+
+// 	}
+
+// }
+// angular.module("button").controller("buttonController", ["$scope", buttonController])
+
 angular.module("button", [])
 
 
 var buttonController = function ($scope){
 	$scope.click = function(){
-		console.log("You clicked me!");
+		$scope.hidden = true;
 	}
+	$scope.click2 = function(){
+		$scope.hidden2 = true;
 }
+	
+	
+}
+
+
 angular.module("button").controller("buttonController", ["$scope", buttonController])
