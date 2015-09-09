c768f9c438af7e9e08ac2716f0e7e4e9f239775f
Student <student@RU-109.local>
2015-09-09 16:41:24 -0600

    4th

---
+++ introang2.js
@@ -0,0 +1,14 @@
+angular.module("myApp", [])
+
+var mouseFunction = function($scope){
+	$scope.mousepink = function(){
+		$scope.
+	}
+	$scope.unpink = function(){
+		$scope.
+	}
+}
+
+
+
+angular.module("myApp").controller("appController", ["$scope", mouseFunction])
