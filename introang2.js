e600caac3a4be36d1a0c3a8d5858c400948eaa8f
Student <student@RU-109.local>
2015-09-10 14:06:05 -0600

    5th

--- introang2.js
+++ introang2.js
@@ -1,14 +1,39 @@
-angular.module("myApp", [])
+var app = angular.module("myApp", [])
 
-var mouseFunction = function($scope){
-	$scope.mousepink = function(){
-		$scope.
-	}
-	$scope.unpink = function(){
-		$scope.
-	}
+// var mouseFunction = function($scope){
+// 	$scope.hoverSelect = function(index){
+// 		$scope.active = index;
+// 	}
+// 	$scope.hoverDeselect = function(index){
+// 		$scope.active = "";
+// 	}
+// }
+
+// var mouseFunction = function($scope){
+// app.directive('hoverClass', function (){
+// 	return {
+// 		restrict: 'A',
+// 		scope: {
+// 			hoverClass: '@'
+// 		},
+// 		link: function (scope, element) {
+// 			element.on('mouseenter', function() {
+// 				element.addClass(scope.hoverClass);
+// 			});
+// 			element.on('mouseleave', function() {
+// 				element.removeClass(scope.hoverClass);
+// 			});
+// 		}
+// 	};
+// });
+// }
+
+var mouseFunction = function(){
+	
 }
 
 
+	
 
-angular.module("myApp").controller("appController", ["$scope", mouseFunction])
+
+app.controller("appController", ["$scope", mouseFunction])
